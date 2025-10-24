5feaa0e178c8980c9a9166816dc8439234886d07
parvajio <mdparvajmosharof17@gmail.com>
2025-10-24 22:12:13 +0600

    sign up and other auth design

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <div>Hello World!</div>
+      <div >Hello World!</div>
       <Button></Button>
     </>
   );
