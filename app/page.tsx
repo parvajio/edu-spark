df9560c45688bbd42c8e4920fd595301a537777e
parvajio <mdparvajmosharof17@gmail.com>
2025-10-24 23:28:16 +0600

    db applied

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <div >Hello World!</div>
       <Button></Button>
+      <UserButton></UserButton>
     </>
   );
 }
