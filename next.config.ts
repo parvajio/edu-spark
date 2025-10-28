59d93a76e27b48ab3487ccd9aad20523b62b3930
MD. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-10-28 05:38:59 +0000

    next config for github codespace

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        '.app.github.dev',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
