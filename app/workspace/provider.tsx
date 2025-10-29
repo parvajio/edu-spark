c7036ace09b01130f0ab09bd99e854fd45680058
MD. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-10-29 08:06:41 +0000

    sidebar from shadcn

--- app/workspace/provider.tsx
+++ app/workspace/provider.tsx
@@ -1,7 +1,13 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import React from "react";
 
 const WorkspaceProvider = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider>
+        <SidebarTrigger />
+      <div>{children}</div>;
+    </SidebarProvider>
+  );
 };
 
 export default WorkspaceProvider;
