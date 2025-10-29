5c9ff48013395ec7c76a6eefab6da3e3329d3e5d
MD. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-10-29 07:53:32 +0000

    user details context

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,27 +1,33 @@
-"use client"
+"use client";
+import { UserDetailsContext } from "@/context/userDetailsContext";
 import { useUser } from "@clerk/nextjs";
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-    const {user} = useUser()
+  const { user } = useUser();
 
-    useEffect(()=>{
-        createUser()
-    },[user])
+  const [userDetails, setUserDetails] = useState();
 
+  useEffect(() => {
+    createUser();
+  }, [user]);
 
-    const createUser = async ()=>{
-        if (!user) return; 
-        
-        const res = await axios.post("/api/user", {
-            name: user.fullName,
-            email: user.primaryEmailAddress?.emailAddress // Get the first email address
-        })
-        console.log(res.data)
-    }
+  const createUser = async () => {
+    if (!user) return;
 
-  return <div>{children}</div>;
+    const res = await axios.post("/api/user", {
+      name: user.fullName,
+      email: user.primaryEmailAddress?.emailAddress, // Get the first email address
+    });
+    setUserDetails(res.data);
+  };
+
+  return (
+    <UserDetailsContext.Provider value={{ userDetails, setUserDetails }}>
+      {children}
+    </UserDetailsContext.Provider>
+  );
 };
 
 export default Provider;
