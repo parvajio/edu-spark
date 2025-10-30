605574420ba13dc646736ed322cc80182b3e4551
parvajio <mdparvajmosharof17@gmail.com>
2025-10-30 15:50:29 +0600

    courselist creation design

--- app/workspace/_components/CourseList.tsx
+++ app/workspace/_components/CourseList.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import React, { useState } from 'react'
 
 const CourseList = () => {
+  const [courseList, setCourseList] = useState([]);
   return (
     <div>
-      cou
+      <h2 className='font-bold text-2xl my-5'>Course List</h2>
+      {courseList.length == 0 ? (
+        <div className='w-full py-10 bg-gray-200 flex flex-col justify-center items-center rounded-md gap-3'>
+          <p>Look like you haven't created any courses yet</p>
+          <Button asChild>
+            <Link href={'/'}>+ Create Your first course</Link>
+          </Button>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   )
 }
--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import WelcomeDashboard from './_components/WelcomeDashboard'
+import CourseList from './_components/CourseList'
 
 const page = () => {
   return (
     <div>
       <WelcomeDashboard></WelcomeDashboard>
+      <CourseList></CourseList>
     </div>
   )
 }
